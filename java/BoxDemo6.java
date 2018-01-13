c20813eba7f1553246fa306f0bd86e4dda7047bc
Paulmburu <paulmburu53@gmail.com>
2018-01-13 11:04:55 +0300

    Java complete reference

---
+++ java/BoxDemo6.java
@@ -0,0 +1,41 @@
+/* Here, Box uses a constructor to initialize the
+ * dimensions of a box.
+ */
+
+class Box {
+	double width;
+	double height;
+	double depth;
+
+
+	// This is the constructor for Box.
+	Box(){
+		System.out.println("Constructing Box");
+		width = 10;
+		height = 10;
+		depth =10;
+	}
+
+	// compute and return volume
+	double volume() {
+		return width * height *depth;
+	}
+}
+
+class BoxDemo6 {
+	public static void main(String args[]){
+		// declare, allocate and initiliaze Box objects
+		Box myBox1 = new Box();
+		Box myBox2 = new Box();
+
+		double vol;
+
+		//get volume of the first box
+		vol = myBox1.volume();
+		System.out.println("Volume is " + vol);
+
+		//get volume of the second box
+		vol = myBox2.volume();
+		System.out.println("Volume is " + vol);
+	}
+}
