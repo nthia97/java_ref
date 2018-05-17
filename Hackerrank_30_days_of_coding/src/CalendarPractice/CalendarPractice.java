282082bf96ce08617a4789a2e688722716495ca4
Paulmburu <paulmburu53@gmail.com>
2018-05-18 00:55:00 +0300

    ...

--- Hackerrank_30_days_of_coding/src/CalendarPractice/CalendarPractice.java
+++ Hackerrank_30_days_of_coding/src/CalendarPractice/CalendarPractice.java
@@ -9,9 +9,13 @@ public class CalendarPractice {
     public static void main(String[] args) {
         Calendar calendar=Calendar.getInstance();
         calendar.add(Calendar.DATE,0);
+
+
+        System.out.println("this->"+Calendar.DATE);
         System.out.println(calendar.getTime());
         SimpleDateFormat format1=new SimpleDateFormat("yyyy-MM-dd");
         String formatted=format1.format(calendar.getTime());
         System.out.println(formatted);
+
     }
 }
