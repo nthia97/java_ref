5e8bdce9b6e4cda2ae37a6b7fbb336acc5a5db8f
Paulmburu <paulmburu53@gmail.com>
2018-06-03 22:47:45 +0300

    generics

--- Hackerrank_30_days_of_coding/src/Generics/Generics.java
+++ Hackerrank_30_days_of_coding/src/Generics/Generics.java
@@ -10,7 +10,7 @@ public class Generics {
     }
 
     public static <T extends Comparable<T>> T findMax(T a,T b){
-        int n=a.compareTo(b);
+        int n=a.compareTo(b) ;
         if(n<0){
             return b;
         }else{
@@ -25,5 +25,9 @@ public class Generics {
         System.out.println(findMax(2,3));
         System.out.println(findMax(3,3));
         System.out.println(findMax(3,2));
+
+        System.out.println(findMax(2.0,3.0));
+        System.out.println(findMax('a','b'));
+        System.out.println(findMax("hello","there"));
     }
 }
