2a84b9e8672babf0186950cd0dfe2afbac44f5e5
Paulmburu <paulmburu53@gmail.com>
2018-09-12 10:09:30 +0300

    Timer and Timertask class first commit

---
+++ PART2/chapter_19/TimerAndTimetask/TTest.java
@@ -0,0 +1,29 @@
+// Demonstrate Timer and TimerTask.
+
+import java.util.*;
+
+class MyTimerTask extends TimerTask {
+	int count=0;
+	public void run(){
+		System.out.println("Timer task executed."+count);
+		count++;
+	}
+}
+
+class TTest {
+	public static void main(String[] args) {
+		MyTimerTask myTask = new MyTimerTask();
+		Timer myTimer = new Timer();
+
+		/* Set an initial delay of 1 second,
+		then repeat every half second.
+		*/
+		myTimer.schedule(myTask, 1000, 500);
+
+		try{
+			Thread.sleep(5000);
+		}catch(InterruptedException exc){}
+
+		myTimer.cancel();
+	}
+}
